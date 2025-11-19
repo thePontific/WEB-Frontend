b8473db4e260df6aa078ab97da895b562adff746
Nastya <epic_star@mail.ru>
2025-11-19 15:23:35 +0300

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,29 @@
+// service-worker.js - улучшенная версия
+const CACHE_NAME = 'galaxy-app-v1';
+const urlsToCache = [
+  '/WEB-Frontend/',
+  '/WEB-Frontend/index.html',
+  '/WEB-Frontend/static/js/bundle.js',
+  '/WEB-Frontend/static/css/main.css',
+  // добавьте другие важные ресурсы
+];
+
+self.addEventListener('install', event => {
+  console.log('Service Worker installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
 self.addEventListener('fetch', event => {
-  console.log("Fetch intercepted for:", event.request.url);
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Возвращаем кэшированную версию или делаем запрос
+        return response || fetch(event.request);
+      })
+  );
 });
