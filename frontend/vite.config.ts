e19de978575e7593b1b25c6b39a88c33bb8c9742
Nastya <epic_star@mail.ru>
2025-11-19 10:17:07 +0300

    Add HTTPS config and PWA setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
   base: "/WEB-Frontend/",
-  server: { 
+  plugins: [react()],
+  server: {
     port: 3000,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
@@ -12,5 +19,4 @@ export default defineConfig({
       },
     }
   },
-  plugins: [react()],
 })
